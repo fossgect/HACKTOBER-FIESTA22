47860c99f6ea3a2e6f304197a5dceceb6af15b2b
MOHAMMED SHIBIN V <md.shibin.vgl@gmail.com>
2022-10-18 21:06:30 +0530

    added snake game

--- pygame/main.py
+++ pygame/main.py
@@ -1,9 +1,10 @@
 from gameit.example import exampleGame
 from gameit.aqeelshamz import treasureHunt
+from gameit.MohdShibin import snakeGame
 
 
 def main():
-    games = {"example": exampleGame, "aqeelshamz": treasureHunt}
+    games = {"example": exampleGame, "aqeelshamz": treasureHunt, "MohdShibin": snakeGame}
     while True:
         username = input("Enter github username:\n")
         try:
