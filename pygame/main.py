7e6a4dbdee5df01978f8c2807f703fb2ea558ada
TomBenDev <116013877+TomBenDev@users.noreply.github.com>
2022-10-18 23:29:20 +0530

    updated path

--- pygame/main.py
+++ pygame/main.py
@@ -1,9 +1,10 @@
 from gameit.example import exampleGame
 from gameit.aqeelshamz import treasureHunt
+from gameit.TomBenDev import pongGame
 
 
 def main():
-    games = {"example": exampleGame, "aqeelshamz": treasureHunt}
+    games = {"example": exampleGame, "aqeelshamz": treasureHunt, "TomBenDev": pongGame}
     while True:
         username = input("Enter github username:\n")
         try:
