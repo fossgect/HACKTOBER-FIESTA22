0fb669f43141cf71de5bc654ac9d0926b4f6b9c2
Sidharth Murali <kmsidharthmurali@gmail.com>
2022-10-21 17:21:37 +0530

    Documentation has been done

--- pygame/gameit/kmSidharthM.py
+++ pygame/gameit/kmSidharthM.py
@@ -1,3 +1,22 @@
+# Space Invader
+# Created by Sidharth Murali
+
+#  =>The aim of the game is to kill all the enemies coming towards space to attack using a spaceship.
+
+# How to play the game:
+#   ->  Use the arrow keys to move the spaceship left and right
+#   ->  Press spacebar to fire.
+
+# Points to be noted:
+#   ->  At a time 2 enemies are spawned at random horizontal position and are headed downwards, 
+#       killing one of them creates a new enemy. 
+#   ->  Even if one of the enemy enters the space,preventing the other enemy to enter can take the 
+#       game forward. Once both the enemies enters the space the game gets over with a score same 
+#       as number of enemies killed.
+#   ->  When score is at every multiple of 15 the downward velocity of the enemies increases thus increasing difficulty.
+#   ->  Only 1 bullet at a time that is you will not be able to fire until the fired bullet leaves the game window
+
+
 import pygame
 import time
 import random
@@ -10,14 +29,14 @@ def Space_Invader():
 
     screen=pygame.display.set_mode((500,500))
     pygame.display.set_caption('SPACE INVADER')
-    # icon=pygame.image.load('static/spaceship.png')
-    # pygame.display.set_icon(icon) 
+    icon=pygame.image.load('static/kmSidharthM/spaceship.png')
+    pygame.display.set_icon(icon) 
     gameState=True
     value=random.randint(23,28)
     while(gameState):
 
         #player details
-        playerimg=pygame.image.load('static/player.png')
+        playerimg=pygame.image.load('static/kmSidharthM/player.png')
         playerx=218
         playery=430
         playerchangex=0
@@ -29,12 +48,12 @@ def Space_Invader():
         noofEnemy=2
         enemyspeed=0.05
         for i in range(2):
-            enemyimg.append(pygame.image.load('static/enemy.png'))
+            enemyimg.append(pygame.image.load('static/kmSidharthM/enemy.png'))
             enemyx.append(random.randint(0,436))
             enemyy.append(100)
 
         #bullet details
-        bulletimg=pygame.image.load('static/bullet.png')
+        bulletimg=pygame.image.load('static/kmSidharthM/bullet.png')
         bulletx=playerx
         bullety=430
         bulletchange=0.4
@@ -42,7 +61,7 @@ def Space_Invader():
 
 
         score=0
-        font=pygame.font.Font('static/BebasNeue-Regular.ttf',22)
+        font=pygame.font.Font('static/kmSidharthM/BebasNeue-Regular.ttf',22)
         scorex=10
         scorey=10
 
@@ -50,12 +69,12 @@ def Space_Invader():
         #gameover text
         gm='GAME OVER'
         restart='Press Esc to continue'
-        gmtext=pygame.font.Font('static/Golden Age Shad.ttf',64)
-        restarttext=pygame.font.Font('static/Golden Age Shad.ttf',26)
+        gmtext=pygame.font.Font('static/kmSidharthM/Golden Age Shad.ttf',64)
+        restarttext=pygame.font.Font('static/kmSidharthM/Golden Age Shad.ttf',26)
 
         running=True
         collision=False
-        background=pygame.image.load('static/background.png')
+        background=pygame.image.load('static/kmSidharthM/background.png')
         
 
         #funtion to display player
@@ -97,7 +116,9 @@ def Space_Invader():
         while running:
             screen.fill((200,200,255))
             screen.blit(background,(0,0))
-            for event in pygame.event.get():
+
+            #reads all pressed key and check for required 
+            for event in pygame.event.get():                            
                 if(event.type==pygame.QUIT):
                     running=False
                     gameState=False
@@ -108,13 +129,16 @@ def Space_Invader():
                         playerchangex=0.15
                     if(event.key==pygame.K_SPACE):
                         if bulletstate == "ready":
-                            mixer.Sound('static/laser.wav').play()
+                            mixer.Sound('static/kmSidharthM/laser.wav').play()
                             bulletx=playerx 
                             bullet(bulletx,bullety)
                 if(event.type==pygame.KEYUP):
                     playerchangex=0
 
+            #edits player position
             playerx+=playerchangex
+
+            #checks for ends of game window
             if(playerx>=436):
                 playerx=436
             if(playerx<=0):
@@ -125,8 +149,8 @@ def Space_Invader():
                 enemyy[i]+=enemyspeed
                 enemy(enemyx[i],enemyy[i],i) 
                 collision=iscollision(enemyx[i]-16,enemyy[i]-16,bulletx-4,bullety-4)
-                if collision:
-                    mixer.Sound('static/explosion.wav').play()
+                if collision:                                                               #checks for collision                  
+                    mixer.Sound('static/kmSidharthM/explosion.wav').play()
                     bullety=430
                     bulletstate="ready"
                     score+=1
@@ -142,6 +166,7 @@ def Space_Invader():
                 bullet(bulletx,bullety)
                 bullety-=bulletchange
             
+            #checks whether both the enemies have entered the space
             if(enemyy[0]>500 and enemyy[1]>500):
                     gameState=False
                     gmfuntion()
