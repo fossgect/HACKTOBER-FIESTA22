7ad214d6dcc196d7bbe768332e4708a4fa7c2847
anuragrajanp <anuragrajanp@gmail.com>
2022-10-22 14:23:26 +0530

    PR 2 - documentation of Feed The Snake
    Documented Feed The Snake game.

--- pygame/gameit/anuragrajanp.py
+++ pygame/gameit/anuragrajanp.py
@@ -1,3 +1,11 @@
+# Feed The Snake is a snake game created using Pygame.
+# 
+# How to play:
+# - Use keyboard arrow keys to move up, down, left and right
+# - Eat the red food to score
+# - Stay away from the walls
+# - Play the game and have fun!
+
 import pygame
 import random
 import time
