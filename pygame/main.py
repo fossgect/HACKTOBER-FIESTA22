213a78966e83044c3836cf56718e39c638a07aeb
unniznd <au6695@gmail.com>
2022-10-23 07:49:44 +0530

    minor fixes

--- pygame/main.py
+++ pygame/main.py
@@ -30,7 +30,7 @@ def main():
         try:
             games[username]()
             print("Happy Gaming")
-        except:
+        except KeyError:
             print("Username not found!!.")
 
 
