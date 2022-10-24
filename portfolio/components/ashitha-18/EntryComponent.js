543d90b5030f71e5ef024d16ae26aff3470fa5fd
ashitha-18 <ashi18pulari@gmail.com>
2022-10-24 23:06:49 +0530

    PR 3 - customised portfolio project

---
+++ portfolio/components/ashitha-18/EntryComponent.js
@@ -0,0 +1,138 @@
+import Image from "next/image";
+import PortfolioNavbar from "./Navbar/Navbar";
+import ProfileImg from '../../public/ashitha-18.jpg' // Change Image name to your Uploaded File Name
+import { useEffect, useState } from "react";
+import { Card, Grid, Text, Row, Button, Link } from "@nextui-org/react";
+
+
+export default function EntryComponent(){
+
+    const [txt,setTxt] = useState("Hey I'm Ashitha P R") // Change X to Your Full Name
+
+    var i = 0;
+    var speed = 100;
+
+    function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("typeTxt").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    }
+
+    useEffect(()=>{
+        typeWriter()
+        setTxt("")
+    })
+
+    return(
+        <div className="">
+
+            {/* Navbar */}  
+            <PortfolioNavbar/>
+
+            {/* Hero Section */}
+            <div className="w-full flex flex-col items-center py-16 px-8">
+                {/* Profile Image */}
+                <div className="w-fit h-fit max-w-[200px] max-h-[200px] border p-1 border-black rounded-[50%]">
+                    <Image src={ProfileImg} style={{borderRadius: "50%",margin: "0"}} />
+                </div>
+                {/* Description */}
+                <div className="description mt-6">
+                    <div >
+                        <h1 id="typeTxt" className="font-txt text-xl font-extrabold text-center"></h1>
+                    </div>
+                    <p className="text-center font-txt mt-3 max-w-[600px]">
+                        {/* Change Description Here */}
+                        BTech CSE undergrad | budding front-end web developer | web3 enthusiast
+                        <br/><br/>
+                        {/* Add Your Tech Stacks */}
+                        Tech Stacks : HTML, CSS, JavaScript, Python, Java, C, Solidity
+                    </p>
+                </div>
+            </div>
+
+            {/* Project Section */}
+            <div className="w-full pb-8">
+
+                <h1 className="text-center font-bold text-2xl">Projects</h1>
+
+                <div className="grid grid-cols-1 justify-center gap-6 mt-8 lg:grid-cols-2 xl:grid-cols-3">
+
+                    {/* Project 1 */}
+                    <Grid sm={12} md={5} className="flex justify-center">
+                        <Card css={{ width: "330px" }}>
+                        <Card.Header>
+                            <Text b>Text Analyzer</Text>
+                        </Card.Header>
+                        <Card.Divider />
+                        <Card.Body css={{ py: "$10" }}>
+                            <Text>
+                            A simple web app that analyzes the words of an input string and gives
+                            count of characters, words, digits, vowels, consonants etc present in 
+                            the string.
+                            </Text>
+                        </Card.Body>
+                        <Card.Divider />
+                        <Card.Footer>
+                            <Row justify="flex-end">
+                                <Link href="https://github.com/ashitha-18/text-analyzer">
+                                    <Button size="sm" light color="primary">Link</Button>
+                                </Link>
+                            </Row>
+                        </Card.Footer>
+                        </Card>
+                    </Grid>
+
+                    {/* Project 2 */}
+                    <Grid sm={12} md={5} className="flex justify-center">
+                        <Card css={{ width: "330px" }}>
+                        <Card.Header>
+                            <Text b>Personal Assistant - ECHO</Text>
+                        </Card.Header>
+                        <Card.Divider />
+                        <Card.Body css={{ py: "$10" }}>
+                            <Text>
+                                Build a personal assistant using Python as a team of three.
+                            </Text>
+                        </Card.Body>
+                        <Card.Divider />
+                        <Card.Footer>
+                            <Row justify="flex-end">
+                                <Link href="https://github.com/ashitha-18/ECHO---Personal-Assistant">
+                                    <Button size="sm" light color="primary">Link</Button>
+                                </Link>
+                            </Row>
+                        </Card.Footer>
+                        </Card>
+                    </Grid>
+
+                    {/* Project 3 */}
+                    <Grid sm={12} md={5} className="flex justify-center">
+                        <Card css={{ width: "330px" }}>
+                        <Card.Header>
+                            <Text b>Portfolio Website - Artist</Text>
+                        </Card.Header>
+                        <Card.Divider />
+                        <Card.Body css={{ py: "$10" }}>
+                            <Text>
+                                A portfolio website for the artist in me.
+                            </Text>
+                        </Card.Body>
+                        <Card.Divider />
+                        <Card.Footer>
+                            <Row justify="flex-end">
+                                <Link href="https://github.com/ashitha-18/Portfolio-Artist">
+                                    <Button size="sm" light color="primary">Link</Button>
+                                </Link>
+                            </Row>
+                        </Card.Footer>
+                        </Card>
+                    </Grid>
+
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
