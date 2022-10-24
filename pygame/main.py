9110a1387545c1b8d20b96d07e109ea12798c8b5
ashitha-18 <ashi18pulari@gmail.com>
2022-10-24 11:27:20 +0530

    PR 1 - added Sudoko game to pygame

--- pygame/main.py
+++ pygame/main.py
@@ -13,6 +13,7 @@ from gameit.example import exampleGame
 from gameit.ash394 import pongGame
 from gameit.vishakh import car_game
 from gameit.ebinjose02 import Snake
+from gameit.ashitha_18 import SudokoGame
 
 def main():
     games = {
@@ -30,7 +31,8 @@ def main():
       'AnnMol_2002':sudoku,
       "Ash-394": pongGame,
       "vishakh": car_game,
-      "ebinjose02":Snake
+      "ebinjose02":Snake,
+      "ashitha-18":SudokoGame
 }
 
     while True:
