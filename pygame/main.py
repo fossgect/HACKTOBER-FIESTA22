ef2fb9ef5a68f66b2177032c2337fe883096224a
jithinpkumar <jithinpkbtw25@gmail.com>
2022-10-25 23:17:04 +0530

    h

--- pygame/main.py
+++ pygame/main.py
@@ -13,6 +13,7 @@ from gameit.example import exampleGame
 from gameit.ash394 import pongGame
 from gameit.vishakh import car_game
 from gameit.ebinjose02 import Snake
+from gameit.jithinpkumar import ArmyBase
 
 def main():
     games = {
@@ -30,7 +31,8 @@ def main():
       'AnnMol_2002':sudoku,
       "Ash-394": pongGame,
       "vishakh": car_game,
-      "ebinjose02":Snake
+      "ebinjose02":Snake,
+      "jithinpkumar":ArmyBase
 }
 
     while True:
