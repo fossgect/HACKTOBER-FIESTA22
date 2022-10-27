5ef9d5f220c44fa986e8600f0b0e9ff8787b7382
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-10-27 14:09:04 +0530

    4th ts

--- pygame/main.py
+++ pygame/main.py
@@ -23,7 +23,7 @@ from gameit.ashitha_18 import SudokoGame
 from gameit.JP_GECT import sgame
 from gameit.AadeshPS import game1
 from gameit.ezbonpj import bounce
-from gameit.taslimmuhammed import jumpManPro
+from gameit.taslimmuhammed import PlayerPro
 from gameit.nived_krish44 import snake
 
 
@@ -50,7 +50,7 @@ def main():
       "Prometheus2k":Game2048,
       "ashitha-18":SudokoGame,
       "JP_GECT":sgame,
-      "taslimmuhammed":jumpManPro,
+      "taslimmuhammed":PlayerPro,
       "AadeshPS":game1,
       "ezbonpj":bounce,
       "nived_krish44":snake,
