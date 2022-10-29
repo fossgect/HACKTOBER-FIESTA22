67dae596875fb53c62fe6cccdbbca222fd681384
nikhilputhumana <nikhilp.csb1923@saintgits.org>
2022-10-29 13:26:09 +0530

    Documentation for game added

--- pygame/gameit/nikhilputhumana.py
+++ pygame/gameit/nikhilputhumana.py
@@ -1,7 +1,9 @@
-# NUMBER GUESSING GAME
-# User can guess a random number between 1 and 100.
-# Each game has 10 chances and for each chance missed, the points will be decremented by 10.
-# The second level consist of 5 guesses and points decrements by 20 for each wrong guess.
+#   NUMBER GUESSING GAME
+# Users can guess a random number between 1 and 100.
+# In the first level, the game has 10 chances
+# and for each chance missed, the points will be decremented by 10.
+# The second level consist of 5 guesses 
+# and points decrements by 20 for each wrong guess.
 # Messages will be shown based on how close your guess is.
 # The aim is to find the number with least number of guesses.
 
