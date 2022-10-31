6d4820c308523a39d4103d9503d4c40da5975b66
Adethya S Nair <adethyasanjay@gmail.com>
2022-10-31 23:37:05 +0530

    portfolio updated

---
+++ portfolio/pages/nairadethya2208.js
@@ -0,0 +1,10 @@
+import EntryComponent from "../components/nairadethya2208/EntryComponent";
+
+
+export default function Home() {
+  return (
+    <div>
+      <EntryComponent />
+    </div>
+  )
+}
