54be930c268bcd6bc43f499f3c062fb5b37dd07b
Ajmi Nasar <114758659+Ajmi-techie@users.noreply.github.com>
2022-10-31 17:16:57 +0530

    Update Ajmi.py

--- pygame/gameit/Ajmi.py
+++ pygame/gameit/Ajmi.py
@@ -1,3 +1,11 @@
+# car game 
+# project by Ajmi Nasar
+
+# Rules of the game-
+# the objective of the game is to avoid a car crash with careful driving from the user 
+# use the left and right arrow key to make the car change lanes and avoid crashing with the car coming head on
+
+
 import pygame
 from pygame.locals import *
 import random
