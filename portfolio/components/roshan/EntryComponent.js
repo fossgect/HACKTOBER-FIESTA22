2b32a9fa5bd59fef27839ca6310d4045896f1aa3
roshan <tcr21cs040@gectcr.ac.in>
2022-10-31 21:57:34 +0530

    pr-4

--- portfolio/components/roshan/EntryComponent.js
+++ portfolio/components/roshan/EntryComponent.js
@@ -62,18 +62,18 @@ export default function EntryComponent(){
                     <Grid sm={12} md={5} className="flex justify-center">
                         <Card css={{ width: "330px" }}>
                         <Card.Header>
-                            <Text b>Project Name</Text>
+                            <Text b>Snake Game</Text>
                         </Card.Header>
                         <Card.Divider />
                         <Card.Body css={{ py: "$10" }}>
                             <Text>
-                                Project Description
+                                This is a classy game also known as snake xenzia
                             </Text>
                         </Card.Body>
                         <Card.Divider />
                         <Card.Footer>
                             <Row justify="flex-end">
-                                <Link href="https://github.com">
+                                <Link href="https://github.com/rrsshan/HACKTOBER-FIESTA22/blob/main/pygame/gameit/roshan.py">
                                     <Button size="sm" light color="primary">Link</Button>
                                 </Link>
                             </Row>
