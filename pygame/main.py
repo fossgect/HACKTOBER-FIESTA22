f7b4bb498f38e6627f41c612a969ad9eb0958621
Adethya S Nair <adethyasanjay@gmail.com>
2022-10-31 23:20:24 +0530

    documentation for the game is created

--- pygame/main.py
+++ pygame/main.py
@@ -54,6 +54,7 @@ from gameit.Darshana_Das_K import snakeGameWithMenu
 from gameit.BassamEC import pongGame
 from gameit.unniznd import runGame
 from gameit.Chanchalsanthosh import car
+from gameit.nairadethya2208 import game
 
 def main():
     games = {
@@ -112,6 +113,7 @@ def main():
       "Ajmi":racing_game,
       "unniznd":runGame,
       "Chanchalsanthosh":car,
+      "nairadethya2208" : game,
     }
 
 
